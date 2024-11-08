f9530bfffd51ce44a1fc3466bc52d0043dc523ae
airdstar <sam@theconrads.ca>
2024-11-08 11:19:21 -0400

    Moved some stuff back into main

--- floor.gd
+++ floor.gd
@@ -2,21 +2,25 @@ extends Node3D
 
 var camera_sens_controller = 2.5
 var camera_sens_mouse = 0.05
+var control_type : bool = false
+var tilt_sens_controller : float = 1.00
+var tilt_sens_keyboard : float = 0.05
 
-var control_type : bool = true
-
+var proxy_tilt = Node3D.new()
+var input_tilt : Vector2
+var origin_tilt : Vector2
 var max_tilt : float = 15
-var tilt_sens : float = 20
+var tilt_scalar : float = 20
 
 @onready var camera_rot = $camera_y
 @onready var player = $Player
+@onready var origin = $Origin
 
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	var current_platform = preload("res://Platform.tscn").instantiate()
-	current_platform.rot_info = camera_rot
-	add_child(current_platform)
+	origin.add_child(current_platform)
 
 func _process(delta: float) -> void:
 	
@@ -24,6 +28,9 @@ func _process(delta: float) -> void:
 	
 	if Input.is_action_just_pressed("exit"):
 		get_tree().quit()
+	
+	handle_inputs()
+	
 	
 	if control_type:
 		var camera_input = Input.get_axis("camera_left", "camera_right")
@@ -37,6 +44,48 @@ func _process(delta: float) -> void:
 		player.position = Vector3(0,2,0)
 		player.linear_velocity = Vector3.ZERO
 		player.angular_velocity = Vector3.ZERO
+
+func _physics_process(delta: float) -> void:
+	if control_type:
+		
+		if Input.get_axis("tilt_up", "tilt_down") != 0:
+			input_tilt.x += Input.get_axis("tilt_up", "tilt_down") * tilt_sens_controller
+		else:
+			input_tilt.x = 0
+		
+		if Input.get_axis("tilt_right", "tilt_left") != 0:
+			input_tilt.y += Input.get_axis("tilt_right", "tilt_left") * tilt_sens_controller
+		else:
+			input_tilt.y = 0
+		
+	else:
+		
+		if Input.get_axis("tilt_up", "tilt_down") != 0:
+			input_tilt.x += Input.get_axis("tilt_up", "tilt_down") * tilt_sens_keyboard
+		else:
+			input_tilt.x = 0
+		
+		if Input.get_axis("tilt_right", "tilt_left") != 0:
+			input_tilt.y += Input.get_axis("tilt_right", "tilt_left") * tilt_sens_keyboard
+		else:
+			input_tilt.y = 0
+
+	
+	input_tilt.x = clamp(input_tilt.x, -0.45, 0.45)
+	input_tilt.y = clamp(input_tilt.y, -0.45, 0.45)
+	
+	origin_tilt.x = clamp(deg_to_rad(input_tilt.x * tilt_scalar), deg_to_rad(-max_tilt), deg_to_rad(max_tilt))
+	origin_tilt.y = clamp(deg_to_rad(input_tilt.y * tilt_scalar), deg_to_rad(-max_tilt), deg_to_rad(max_tilt))
+	
+	proxy_tilt.rotation = camera_rot.transform.basis * Vector3(origin_tilt.x, 0, origin_tilt.y)
+	
+	var a = Quaternion(origin.transform.basis)
+	var b = Quaternion(proxy_tilt.transform.basis)
+	var c = a.slerp(b,0.15)
+	origin.transform.basis = Basis(c)
+
+func handle_inputs():
+	pass
 
 func _input(event: InputEvent):
 	if event is InputEventMouseMotion:
