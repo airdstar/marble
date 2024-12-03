82005f1b14c5478673ac9754703e44d694237cc5
airdstar <sam@theconrads.ca>
2024-12-02 20:25:31 -0400

    Fixed issue where gallery lagged

--- Main/gallery.gd
+++ Main/gallery.gd
@@ -1,20 +1,16 @@
 extends Node
 
-var allow_test : bool = false
-
 var easy_veto : bool = false
 var medium_veto : bool = false
 var hard_veto : bool = false
 
-var selected_tab : int = 0
+var selected_tab : int = 1
 var options : Array[Button] = []
-var option_directories : Array[String] = []
-
+var option_info : Array[level_resource] = []
 
 @onready var easy : Button = $VBoxContainer/HBoxContainer/Easy
 @onready var medium : Button = $VBoxContainer/HBoxContainer/Medium
 @onready var hard : Button = $VBoxContainer/HBoxContainer/Hard
-@onready var test : Button = $VBoxContainer/HBoxContainer/Test
 
 func _ready():
 	check_valid_difficulties()
@@ -22,75 +18,63 @@ func _ready():
 
 func show_options():
 	clear_options()
-	var dir = DirAccess.open(Global.level_directories[selected_tab])
+	var dir = DirAccess.open("res://Levels/Info/")
 	dir.list_dir_begin()
 	var currentLevel : String = dir.get_next()
-	var allLevels : Array[String]
 	while currentLevel != "":
-		var holder = Button.new()
-		holder.size_flags_vertical = 3
-		options.append(holder)
-		$VBoxContainer.add_child(holder)
-		if '.tscn.remap' in currentLevel:
+		if '.tres.remap' in currentLevel:
 			currentLevel = currentLevel.trim_suffix('.remap')
-		allLevels.append(Global.level_directories[RunInfo.currentDifficulty] + currentLevel)
+		
+		if currentLevel.begins_with(str(selected_tab)):
+			
+			var resource_holder = ResourceLoader.load("res://Levels/Info/" + currentLevel)
+			
+			option_info.append(resource_holder)
+			
+			var button_holder = Button.new()
+			button_holder.text = resource_holder.tagline + "   ID: " + str(currentLevel.trim_suffix('.tres')) 
+			options.append(button_holder)
+			$VBoxContainer/ScrollContainer/VBoxContainer.add_child(button_holder)
 		
 		currentLevel = dir.get_next()
-		
+	
 	dir.list_dir_end()
-	return allLevels[randi_range(0, allLevels.size() - 1)]
+
 
 func clear_options():
 	for n in options:
 		n.queue_free()
 	options.clear()
-	option_directories.clear()
+	option_info.clear()
 
 func check_valid_difficulties() -> void:
-	for n in range(4):
-		var level_count : int = 0
-		var dir : DirAccess = DirAccess.open(Global.level_directories[n])
-		if dir != null:
-			dir.list_dir_begin()
-			var currentLevel : String = dir.get_next()
-			while currentLevel != "":
-				level_count += 1
-				currentLevel = dir.get_next()
-			dir.list_dir_end()
-		match n:
-			0:
-				if level_count > 0 and !easy_veto:
-					easy.disabled = false
-				else:
-					easy.disabled = true
-			1:
-				if level_count > 0 and !medium_veto:
-					medium.disabled = false
-				else:
-					medium.disabled = true
-			2:
-				if level_count > 0 and !hard_veto:
-					hard.disabled = false
-				else:
-					hard.disabled = true
-			3:
-				if allow_test:
-					if level_count > 0:
-						test.disabled = false
-					else:
-						test.disabled = true
-				else:
-					test.visible = false
-
+	var easy_enabled : bool = false
+	var medium_enabled : bool = false
+	var hard_enabled : bool = false
+	
+	var dir = DirAccess.open("res://Levels/Info/")
+	dir.list_dir_begin()
+	var currentLevel : String = dir.get_next()
+	while currentLevel != "":
+		if currentLevel.begins_with(str(1)) and !easy_enabled:
+			easy_enabled = true
+			easy.visible = true
+		elif currentLevel.begins_with(str(2)) and !medium_enabled:
+			medium_enabled = true
+			medium.visible = true
+		elif currentLevel.begins_with(str(3)) and !hard_enabled:
+			hard_enabled = true
+			hard.visible = true
+		currentLevel = dir.get_next()
 
 func easy_pressed() -> void:
-	selected_tab = 0
-	show_options()
-
-func medium_pressed() -> void:
 	selected_tab = 1
 	show_options()
 
-func hard_pressed() -> void:
+func medium_pressed() -> void:
 	selected_tab = 2
+	show_options()
+
+func hard_pressed() -> void:
+	selected_tab = 3
 	show_options()
---
+++ Resources/LevelResource.gd
@@ -0,0 +1,20 @@
+extends Resource
+class_name level_resource
+
+@export var tagline : String = "Test"
+
+
+@export var given_time : float = 5.0
+@export var start_pos : Array[Vector2] = [Vector2.ZERO]
+@export var possible_rotations : Vector2 = Vector2(0,360)
+
+@export_enum("Fall", "Paint") var goal_type : String = "Fall"
+
+@export var associated_scene : PackedScene 
+
+@export_category("Experimental")
+
+@export var max_tilt : float = 35
+@export var gravity : Vector3 = Vector3(0, 9.8, 0)
+
+@export var end_pos : Array[Vector2]
