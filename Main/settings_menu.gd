0e505798cae03104159f090ba9702f1498114133
airdstar <sam@theconrads.ca>
2024-12-10 20:46:43 -0400

    Updated settings

--- Main/settings_menu.gd
+++ Main/settings_menu.gd
@@ -1,10 +1,12 @@
 extends Node
 
-@onready var camera_slider : HSlider = $CameraSlider
-@onready var deadzone_slider : HSlider = $DeadzoneSlider
+@onready var tilt_slider : HSlider = $ControlSettingsBox/TiltSens/HSplitContainer/TiltSlider
+@onready var camera_slider : HSlider = $ControlSettingsBox/CameraSens/HSplitContainer/CameraSlider
+@onready var deadzone_slider : HSlider = $ControlSettingsBox/Deadzone/HSplitContainer/DeadzoneSlider
 
-@onready var deadzone_label : RichTextLabel = $DeadzoneSlider/DeadzoneValue
-@onready var camera_sens_label : RichTextLabel = $CameraSlider/CameraSensValue
+@onready var tilt_value : RichTextLabel = $ControlSettingsBox/TiltSens/TiltSensValue
+@onready var camera_value : RichTextLabel = $ControlSettingsBox/CameraSens/CameraSensValue
+@onready var deadzone_value : RichTextLabel = $ControlSettingsBox/Deadzone/DeadzoneValue
 
 @onready var red_slider : HSlider = $Red
 @onready var blue_slider : HSlider = $Blue
@@ -27,32 +29,18 @@ func _process(_delta: float) -> void:
 
 
 func set_slider_values() -> void:
-	camera_slider.min_value = 60
-	camera_slider.max_value = 90
-	camera_slider.step = 0.5
 	camera_slider.value = settings.camera_sens
-		
-	deadzone_slider.min_value = 1
-	deadzone_slider.max_value = 40
-	deadzone_slider.step = 0.5
 	deadzone_slider.value = settings.tilt_deadzone
-	
-	
+
 
 func tilt_sens_changed(value : float) -> void:
-	#settings.tilt_sens = value
-	pass
+	tilt_value.text = "[center]" + str(snapped((value - tilt_slider.min_value)/tilt_slider.min_value * 2,0.01))
 
 func camera_sens_changed(value: float) -> void:
-	settings.camera_sens = value
-	camera_sens_label.text = "[center]" + str(snapped((value - camera_slider.min_value)/camera_slider.min_value * 2,0.01))
+	camera_value.text = "[center]" + str(snapped((value - camera_slider.min_value)/camera_slider.min_value * 2,0.01))
 
 func deadzone_changed(value: float) -> void:
-	settings.tilt_deadzone = value
-	deadzone_label.text = "[center]" + str(snapped((value / deadzone_slider.max_value), 0.01))
-
-func close_pressed() -> void:
-	Global.main.close_settings()
+	deadzone_value.text = "[center]" + str(snapped((value / deadzone_slider.max_value), 0.01))
 
 func red_changed(value: float) -> void:
 	PlayerInfo.player_data.player_color.r = value
@@ -68,3 +56,15 @@ func green_changed(value: float) -> void:
 	PlayerInfo.player_data.player_color.g = value
 	$SubViewport/MeshInstance3D.mesh.material.albedo_color = PlayerInfo.player_data.player_color
 	$SubViewport/MeshInstance3D/OmniLight3D.light_color = PlayerInfo.player_data.player_color
+
+func save_settings() -> void:
+	settings.camera_sens = camera_slider.value
+	settings.tilt_deadzone = deadzone_slider.value
+
+func reset_settings() -> void:
+	PlayerInfo.player_data.player_settings = Settings.new()
+	set_slider_values()
+
+func close_pressed() -> void:
+	Global.main.close_settings()
+	set_slider_values()
