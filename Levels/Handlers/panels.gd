b4d275015cbd092ec3127a9d35f2a17f9520f7fd
airdstar <sam@theconrads.ca>
2025-02-04 23:07:53 -0400

    Upscaled the test face

--- Levels/Handlers/panels.gd
+++ Levels/Handlers/panels.gd
@@ -16,7 +16,7 @@ func _ready() -> void:
 		for n in columns:
 			var holder = MeshInstance3D.new()
 			holder.mesh = ResourceLoader.load("res://Levels/Parts/Panel.obj")
-			holder.position = Vector3(-cos(PI * (n + (float(m) / 3)) / (columns / 2)) * (20 - float(m) / 5), 12.5 - m * 2, sin(PI * (n + (float(m) / 3)) / (columns / 2)) * (20 - float(m) / 5))
+			holder.position = Vector3(-cos(PI * (n + (float(m) / 3)) / (columns / 2)) * (20 - float(m) / 5), 11 - m * 2, sin(PI * (n + (float(m) / 3)) / (columns / 2)) * (20 - float(m) / 5))
 			holder.rotation = Vector3(0, PI * 2.0 * (float(n + float(m) / 3) / columns), deg_to_rad(20))
 			holder.set_layer_mask_value(7, true)
 			holder.set_layer_mask_value(1, false)
