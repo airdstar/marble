9d3a44677bb17ff0a7ad524556cdf2f63b0a521d
airdstar <sam@theconrads.ca>
2025-10-22 14:20:59 -0300

    Updated profile

--- Main/Menus/profile.gd
+++ Main/Menus/profile.gd
@@ -1,12 +1,21 @@
 extends Control
 
-func _process(_delta: float) -> void:
+var player_dummy : player
+
+func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("back"):
 		Global.close_popup()
+	
+	if player_dummy != null:
+		player_dummy.rotate_y(0.5 * delta)
 
 func set_data(playerdata : PlayerData) -> void:
 	%Runs.text = str(playerdata.game_over_count)
 	%Highest.text = str(playerdata.highest_level)
+	player_dummy = preload("res://Main/Player.tscn").instantiate()
+	player_dummy.set_customization(playerdata.player_customization)
+	player_dummy.freeze = true
+	$SubViewport.add_child(player_dummy)
 
 func close_pressed() -> void:
 	queue_free()
