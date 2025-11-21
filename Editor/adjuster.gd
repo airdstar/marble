337f1c1d62b00ef29c93f5f415189e4ac3b740bd
airdstar <sam@theconrads.ca>
2025-11-21 13:23:17 -0400

    Updated movement tool to not be 3d based (works better)

--- Editor/adjuster.gd
+++ Editor/adjuster.gd
@@ -1,12 +1,5 @@
 extends Node3D
 
-enum axis {
-	NONE,
-	X,
-	Y,
-	Z
-}
-
 enum state {
 	IDLE,
 	GRAB
@@ -20,29 +13,124 @@ enum tool {
 	
 }
 
+const RAY_LENGTH := 100
+
 var current_tool : tool = tool.NONE
 var current_state : state = state.IDLE
-var grabbed : axis = axis.NONE
-var hovered : axis = axis.NONE
+var grabbed : int = -1
+var hovered : int = -1
 
-func _physics_process(_delta: float) -> void:
+@onready var axies : Array = [$X, $Y, $Z]
+var axies_base : Array[Color] = [Color(1.0, 0.1, 0.1, 1.0), Color(0.1, 1.0, 0.1, 1.0), Color(0.1, 0.1, 1.0, 1.0)]
+var axies_hover : Array[Color] = [Color(1.0, 0.4, 0.4, 1.0), Color(0.4, 1.0, 0.4, 1.0), Color(0.4, 0.4, 1.0, 1.0)]
+
+var adj_pos : Array[float]
+var adj_scale : Array[float]
+var adj_rot : Array[float]
+
+
+var initial_mouse_pos : Vector2
+var initial_pos : Array[float]
+var inital_rot : Vector3
+var initial_scale : Vector3
+var prev_mouse_pos : Vector2
+
+var left_right_scale : float
+var up_down_scale : float
+var distance_scale : float = 1
+
+func _ready() -> void:
+	pass
+
+func _process(_delta: float) -> void:
+	if %Info.adjusting == editor_info.adjustable.NONE:
+		return
+	set_adj()
+	update_pos()
+	update_rot()
+	if grabbed != -1:
+		match current_tool:
+			tool.POS:
+				grab_pos()
+			tool.ROT:
+				pass
+			tool.SCALE:
+				pass
+	
+	prev_mouse_pos = get_viewport().get_mouse_position()
+
+func grab_pos() -> void:
+	if initial_mouse_pos == get_viewport().get_mouse_position():
+		return
+	var dif = initial_mouse_pos - get_viewport().get_mouse_position()
+	adj_pos[grabbed] = (initial_pos[grabbed] + (dif.x * left_right_scale  * distance_scale + dif.y * up_down_scale * distance_scale * 10))
+	adj_pos[grabbed] = snapped(adj_pos[grabbed], 0.1)
+	%Change.position(Vector3(adj_pos[0], adj_pos[1], adj_pos[2]))
+
+
+func grab_rot() -> void:
+	pass
+
+func grab_scale() -> void:
+	pass
+
+
+func set_adj() -> void:
+	adj_pos.clear()
+	adj_scale.clear()
+	adj_rot.clear()
 	match %Info.adjusting:
 		editor_info.adjustable.PART:
-			position = %Info.selected_part.position
+			adj_pos.append_array([%Info.selected_part.position.x, %Info.selected_part.position.y, %Info.selected_part.position.z])
+			adj_scale.append_array([%Info.selected_part.scale.x, %Info.selected_part.scale.y, %Info.selected_part.scale.z])
+			adj_rot.append_array([%Info.selected_part.rotation.x, %Info.selected_part.rotation.y, %Info.selected_part.rotation.z])
 		editor_info.adjustable.SHAPE:
-			position = %Info.selected_shape.total_offset
-	
-	if current_tool == tool.SCALE:
-		match %Info.adjusting:
-			editor_info.adjustable.PART:
-				$X.position.x = %Info.selected_part.scale.x / 2
-				$Y.position.y = %Info.selected_part.scale.y / 2
-				$Z.position.z = %Info.selected_part.scale.z / 2
-			editor_info.adjustable.SHAPE:
-				$X.position.x = %Info.selected_shape.size.x / 2
-				$Y.position.y = %Info.selected_shape.size.y / 2
-				$Z.position.z = %Info.selected_shape.size.z / 2
+			adj_pos.append_array([%Info.selected_shape.total_offset.x, %Info.selected_shape.total_offset.y, %Info.selected_part.total_offset.z])
+			adj_scale.append_array([%Info.selected_part.size.x, %Info.selected_part.size.y, %Info.selected_part.size.z])
+			adj_rot = %Info.selected_shape.rotation.get_axis()
 
+func update_pos() -> void:
+	position = Vector3(adj_pos[0], adj_pos[1], adj_pos[2])
+	if current_tool != tool.NONE:
+		if current_tool == tool.SCALE:
+			axies[0].position.x = adj_scale[0] / 2
+			axies[1].position.y = adj_scale[1] / 2
+			axies[2].position.z = adj_scale[2] / 2
+		else:
+			axies[0].position = Vector3(1,0,0)
+			axies[1].position = Vector3(0,1,0)
+			axies[2].position = Vector3(0,0,1)
+
+func update_rot() -> void:
+	if current_tool != tool.NONE:
+		pass
+
+
+
+func _input(event : InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				grabbed = hovered
+				if grabbed != -1:
+					current_state = state.GRAB
+					initial_mouse_pos = get_viewport().get_mouse_position()
+					initial_pos = [adj_pos[0], adj_pos[1], adj_pos[2]]
+					
+					prev_mouse_pos = get_viewport().get_mouse_position()
+					var object_forward_direction = -axies[grabbed].global_transform.basis.z
+					var direction_to_camera = (get_viewport().get_camera_3d().global_transform.origin - axies[grabbed].global_transform.origin).normalized()
+					var cross_result = object_forward_direction.cross(direction_to_camera)
+					left_right_scale = -cross_result.y
+					up_down_scale = -cross_result.x
+					var distance_from_camera = get_viewport().get_camera_3d().global_position.distance_to(axies[grabbed].global_position)
+					distance_scale = distance_from_camera / 1000
+					
+					update_colors()
+			else:
+				current_state = state.IDLE
+				grabbed = -1
+				update_colors()
 
 func set_tool(_tool : tool) -> void:
 	current_tool = _tool
@@ -52,27 +140,24 @@ func set_visibility() -> void:
 	var visible_mesh : Mesh = null
 	var collider : Shape3D = null
 	if %Info.adjusting != %Info.adjustable.NONE:
+		set_adj()
 		match current_tool:
 			tool.POS:
-				visible_mesh = CylinderMesh.new()
-				visible_mesh.height = 1.8
-				visible_mesh.top_radius = 0
-				visible_mesh.bottom_radius = 0.1
-				visible_mesh.radial_segments = 4
-				
-				$X.position = Vector3(1,0,0)
-				$Y.position = Vector3(0,1,0)
-				$Z.position = Vector3(0,0,1)
-				
-				$X.rotation_degrees = Vector3(0,0,-90)
-				$Y.rotation = Vector3.ZERO
-				$Z.rotation_degrees = Vector3(90,0,0)
+				visible_mesh = BoxMesh.new()
+				visible_mesh.size = Vector3(0.1, 0.1, 1.8)
+				$X.look_at(Vector3(-40, adj_pos[1], adj_pos[2]))
+				$Y.look_at(Vector3(adj_pos[0], -40, adj_pos[2]))
+				$Z.look_at(Vector3.FORWARD)
 				
 				collider = BoxShape3D.new()
-				collider.size = Vector3(0.3, 2.0, 0.3)
+				collider.size = Vector3(0.3, 0.3, 2.0)
 				
 			tool.ROT:
-				pass
+				
+				$X.look_at(Vector3(-40, adj_pos[1], adj_pos[2]))
+				$Y.look_at(Vector3(adj_pos[0], -40, adj_pos[2]))
+				$Z.look_at(Vector3.FORWARD)
+				
 			tool.SCALE:
 				visible_mesh = SphereMesh.new()
 				visible_mesh.radius = 0.1
@@ -80,9 +165,9 @@ func set_visibility() -> void:
 				visible_mesh.radial_segments = 16
 				visible_mesh.rings = 8
 				
-				$X.rotation = Vector3.ZERO
-				$Y.rotation = Vector3.ZERO
-				$Z.rotation = Vector3.ZERO
+				$X.look_at(Vector3(-40, adj_pos[1], adj_pos[2]))
+				$Y.look_at(Vector3(adj_pos[0], -40, adj_pos[2]))
+				$Z.look_at(Vector3.FORWARD)
 				
 				collider = BoxShape3D.new()
 				collider.size = Vector3(0.3, 0.3, 0.3)
@@ -92,36 +177,26 @@ func set_visibility() -> void:
 		n.mesh = visible_mesh
 		n.get_child(0).get_child(0).shape = collider
 
-func enter(_axis: String) -> void:
-	match _axis:
-		"X":
-			$X.get_material_override().emission = Color(1.0, 0.4, 0.4, 1.0)
-			hovered = axis.X
-			exit("Y")
-			exit("Z")
-		"Y":
-			$Y.get_material_override().emission = Color(0.4, 1.0, 0.4, 1.0)
-			hovered = axis.Y
-			exit("X")
-			exit("Z")
-		"Z":
-			$Z.get_material_override().emission = Color(0.4, 0.4, 1.0, 1.0)
-			hovered = axis.Z
-			exit("Y")
-			exit("X")
+func update_colors() -> void:
+	var used : int = -1
+	if current_state == state.IDLE:
+		if hovered != -1:
+			axies[hovered].get_material_override().emission = axies_hover[hovered]
+			used = hovered
+	else:
+		if grabbed != -1:
+			axies[grabbed].get_material_override().emission = Color(1,1,1)
+			used = grabbed
+	for n in 3:
+		if n == used:
+			continue
+		axies[n].get_material_override().emission = axies_base[n]
 
+func enter(axis : int) -> void:
+	hovered = axis
+	update_colors()
 
-func exit(_axis: String) -> void:
-	match _axis:
-		"X":
-			$X.get_material_override().emission = Color(1.0, 0.1, 0.1, 1.0)
-			if hovered == axis.X:
-				hovered = axis.NONE
-		"Y":
-			$Y.get_material_override().emission = Color(0.1, 1.0, 0.1, 1.0)
-			if hovered == axis.Y:
-				hovered = axis.NONE
-		"Z":
-			$Z.get_material_override().emission = Color(0.1, 0.1, 1.0, 1.0)
-			if hovered == axis.Z:
-				hovered = axis.NONE
+func exit(axis: int) -> void:
+	if hovered == axis:
+		hovered = -1
+	update_colors()
